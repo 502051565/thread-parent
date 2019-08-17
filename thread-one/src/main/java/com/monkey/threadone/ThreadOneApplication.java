52f36b8e85ca4497c32341ee6d574c75b7ad4f8c
lwy <502051565>
2019-08-17 11:17:41 +0800

    多线程学习初始化提交

---
+++ thread-one/src/main/java/com/monkey/threadone/ThreadOneApplication.java
@@ -0,0 +1,13 @@
+package com.monkey.threadone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadOneApplication.class, args);
+    }
+
+}
